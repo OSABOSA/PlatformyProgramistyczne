550a01c048edff73c02ce45c9a7ca9e3b216e726
OSABOSA <oskar.siemieniuk01@gmail.com>
2024-02-29 08:34:01 +0100

    done

---
+++ PlatformyProgramistyczne/Lab0/FizzBuzz.cs
@@ -0,0 +1,39 @@
+﻿namespace PlatformyProgramistyczne.Lab0
+{
+    internal class FizzBuzz
+    {
+        int limit;
+        public FizzBuzz(int limit)
+        {
+            this.limit = limit;
+        }
+
+        public FizzBuzz()
+        {
+            limit = 100;
+        }
+
+        public void Display()
+        {
+            for (int i = 1; i <= limit; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ PlatformyProgramistyczne/Lab0/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PlatformyProgramistyczne.Lab0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            FizzBuzz fizzBuzz = new FizzBuzz(17);
+            fizzBuzz.Display();
+        }
+    }
+}
+
